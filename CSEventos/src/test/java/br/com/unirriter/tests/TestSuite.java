d00c03a87f6fa6723d007195b98f0cc715424764
Leonardo Sessegolo <leonardo.sessegolo@gmail.com>
2016-10-05 00:14:56 -0300

    Classes de teste incluidas no TestSuite

--- CSEventos/src/test/java/br/com/unirriter/tests/TestSuite.java
+++ CSEventos/src/test/java/br/com/unirriter/tests/TestSuite.java
@@ -3,12 +3,16 @@ package br.com.unirriter.tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import br.com.unirriter.enums.TipoDescontoTest;
 import br.com.unirriter.enums.TipoIngressoTest;
+import br.com.unirriter.service.DescontoEstrategiaPadraoTest;
 import br.com.unirriter.service.EventoServiceTest;
 import br.com.unirriter.utils.DateUtilsTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ 	TipoIngressoTest.class, 
+						TipoDescontoTest.class,
+						DescontoEstrategiaPadraoTest.class,
 						EventoServiceTest.class, 
 						DateUtilsTest.class 
 					})
